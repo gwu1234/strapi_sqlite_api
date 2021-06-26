9689cffa3473dfee59bc66020ee8cd2760cf0c45
Guoping Wu <gwu@Guopings-MacBook-Pro.local>
2021-06-25 21:16:31 -0400

    DATABASE_URL

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,7 +1,7 @@
 const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+  const { host, port, database, user, password } = parse('postgres://sibgexcanumdvc:c7094af5c6db2c1ee022ea7f609a4e2de549e63df23b9602bcb426854683b561@ec2-54-145-224-156.compute-1.amazonaws.com:5432/d3d1vquscpugto');
 
   return {
     defaultConnection: "default",
